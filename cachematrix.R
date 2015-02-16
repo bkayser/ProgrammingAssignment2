979f9cc72ec625be1ab0de29f7949b6984cbdd4d
Bill Kayser <bkayser@newrelic.com>
2015-02-16 14:11:54 -0800

    Solution for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions makeCacheMatrix and cacheSolve together make a
+## utility that caches the inverse of a given matrix, ensuring
+## the calculation will only be performed once.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a wrapper around the given matrix
+## consisting of a list of functions to access both the matrix
+## (e.g., m) and its inverse:
+##
+##   cm <- makeCacheMatrix(m)
+##   m$get           : return the matrix
+##   m$set(x)        : set the matrix to x and clear the inverse
+##   m$setinverse    : set the inverse of the matrix
+##   m$getinverse    : return the inverse of m, or NULL if not set
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(source_matrix = matrix()) {
+    cached_inverse <- NULL
+    get <- function() source_matrix
+    set <- function(new_matrix) {
+        source_matrix <<- new_matrix
+        cached_inverse <<- NULL
+    }
+    setinverse <- function(inverse) cached_inverse <<- inverse
+    getinverse <- function() cached_inverse
+    list(get = get,
+         set = set,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
+## Given an CacheMatrix created with the makeCacheMatrix() function
+## determine the inverse of the underlying data and return it.  If
+## the solution has already been calculated, return the cached value.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix_data, ...) {
+    inverse <- matrix_data$getinverse()
+    if (!is.null(inverse)) {
+        message("getting cached matrix solution")
+    } else {
+        data <- matrix_data$get()
+        inverse <- solve(data, ...)
+        matrix_data$setinverse(inverse)
+    }
+    return(inverse)
 }
